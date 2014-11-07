039b8179ffa29ac15c60e8b6c9a4acaa318a09b3
Kamalkanth Chowdary Adusumilli <kadusumi@indiana.edu>
2014-11-07 10:04:36 -0500

    Implemented test cases
    1)First 2 test cases are basic test cases to check and understand the
    functionality of JUnit framework.
    2)Second set of 2 test cases check whether the html templates
    (index.html and dashboard.html) are being rendered properly or not.

--- FairNShare/test/ApplicationTest.java
+++ FairNShare/test/ApplicationTest.java
@@ -1,23 +1,18 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
-import com.fasterxml.jackson.databind.JsonNode;
+
+import java.util.List;
+
+import models.Person;
+import models.TaskInfo;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
 import org.junit.*;
 
-import play.mvc.*;
-import play.test.*;
-import play.data.DynamicForm;
-import play.data.validation.ValidationError;
-import play.data.validation.Constraints.RequiredValidator;
-import play.i18n.Lang;
-import play.libs.F;
-import play.libs.F.*;
 import play.twirl.api.Content;
-
 import static play.test.Helpers.*;
 import static org.fest.assertions.Assertions.*;
+import play.mvc.Result;
 
 
 /**
@@ -28,18 +23,32 @@ import static org.fest.assertions.Assertions.*;
 */
 public class ApplicationTest {
 
+	@Test
+	  public void sumTest() {     //Sample unit test to check and understand the basics of JUnit 
+	    int a = 1 + 1;            
+	    assertEquals(2, a);       //Check whether a=2 or not
+	  }
+	    
+	  @Test
+	  public void stringTest() {   //Sample unit test to check and understand the basics of JUnit
+	    String str = "Hello world"; 
+	    assertFalse(str.isEmpty()); //Asserts that string is not empty
+	  }
+    
+	//Unit tests to check whether the html pages are being rendered properly or not
+	  
     @Test
-    public void simpleCheck() {
-        int a = 1 + 1;
-        assertThat(a).isEqualTo(2);
+    public void renderIndexTemplateTest() {           //Unit test to check whether index page is rendered properly or not
+        Content html = views.html.index.render("Sign Up.");//Giving Sign Up. as content to be displayed on index page
+        assertThat(contentType(html)).isEqualTo("text/html");//Stating that input content given is either text or html
+        assertThat(contentAsString(html)).contains("Sign Up.");//Checking whether the input given is being rendered on index page or not
     }
 
     @Test
-    public void renderTemplate() {
-        Content html = views.html.index.render("Your new application is ready.");
+    public void renderDashboardTemplateTest() {          //Unit test to check whether dashboard page is rendered properly or not
+        Content html = views.html.dashboard.render("Welcome");
         assertThat(contentType(html)).isEqualTo("text/html");
-        assertThat(contentAsString(html)).contains("Your new application is ready.");
+        assertThat(contentAsString(html)).contains("Welcome");//Checking whether the input given is being rendered on dashboard page or not
     }
 
-
-}
+   }
