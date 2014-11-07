747078e7689891fa1da4b355b8efa54bb1419c67
Aishwarya <aishiyer@indiana.edu>
2014-11-06 21:20:23 -0500

    displayed the user points in dashboard page as well as notifications page.. modified logout button css

--- FairNShare/app/assets/new.js
+++ FairNShare/app/assets/new.js
@@ -3,9 +3,35 @@ $(document).ready(
         $.get("/getPointsToComplete",function(data,status){
              $("#toComplete").text(data.PointsToComplete);
              $("#earned").text(data.EarnedPoints);
-                     });
+                     });    
         
-    }
+        
+$("#notifications").click(function(){
+  $("#task_div").hide();
+  $('#friend_div').hide();
+  $("#dashboard_div").hide();
+  $.get("/getPointsToComplete",function(data,status){
+     $("#toComplete").text(data.PointsToComplete);
+     $("#earned").text(data.EarnedPoints);
+     $("#userPoints").show();
+     $("#userPoints").prepend("<h2 id='notification_div' style='top:70px;'>Notifications</h2>");
+
+           });
+        });        
+
+$("#dashboard_nav").click(function(){
+      $("#task_div").hide();
+      $("#friend_div").hide();
+      $("#notification_div").hide();
+      $("#dashboard_div").show();
+      $.get("/getPointsToComplete",function(data,status){
+      $("#toComplete").text(data.PointsToComplete);
+      $("#earned").text(data.EarnedPoints);
+      $("#userPoints").show();
+               });
+         }); 
+    });
+    
+
     
     
-);
