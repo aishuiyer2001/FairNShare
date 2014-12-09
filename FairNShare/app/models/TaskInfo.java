396e4c7ee72011c99022e743e00f3138cdd388e6
GAUTAM POORNACHANDRA <gpoornac@indiana.edu>
2014-12-08 19:55:45 -0500

    Iteration2Code_With_Schema_Changes
    Schema changes to scale to the iteration3 requirements.

--- FairNShare/app/models/TaskInfo.java
+++ FairNShare/app/models/TaskInfo.java
@@ -10,53 +10,30 @@ import play.data.validation.Constraints.Required;
 import play.db.ebean.Model;
 @SuppressWarnings("serial")
 @Entity
-public class TaskInfo extends Model
-{
+public class TaskInfo extends Model{
 
 	@Id
 	private long taskID;
-
+	
 	@Required(message = "validation.required.emphasis")
 	private String title;
-
+	
 	private String description;
-
+	
 	private String createdBy;
-
+	
 	@ManyToOne
 	@Email
+	@Required(message = "validation.required.emphasis")
 	@JoinColumn(name = "emailAssignedTo", referencedColumnName = "email")
 	private String emailAssignedTo;
-
+	
 	@Required
 	private boolean done;
-
+	
 	private boolean recurring_status;
-
-	//@DateTime(pattern = "mm/dd/yyyy hh:mm")
-	private String startDate;
-
-
-	//@DateTime(pattern = "mm/dd/yyyy hh:mm")
-	private String endDate;
-
-	@Required
-	private int oldPoints;
-
-	@Required
-	private int newPoints;
-
-
-
-	public TaskInfo()
-	{
-		this.done=false;
-		this.recurring_status=false;
-		this.startDate=null;
-		this.endDate=null;
-		this.emailAssignedTo=null;
-	}
-
+	
+	
 	public boolean isRecurring_status() {
 		return recurring_status;
 	}
@@ -65,23 +42,15 @@ public class TaskInfo extends Model
 		this.recurring_status = recurring_status;
 	}
 
-	public int getOldPoints() {
-		return oldPoints;
+	public TaskInfo()
+	{
+	this.done=false;
+	this.points=0;
 	}
-
-	public void setOldPoints(int oldPoints) {
-		this.oldPoints = oldPoints;
-	}
-
-	public int getnewPoints() {
-		return newPoints;
-	}
-
-	public void setNewPoints(int newPoints) {
-		this.newPoints = newPoints;
-	}
-
-	public long getTaskID() {
+	
+	
+	
+	 public long getTaskID() {
 		return taskID;
 	}
 
@@ -110,8 +79,7 @@ public class TaskInfo extends Model
 	}
 
 	public void setEmailAssignedTo(String emailAssignedTo) {
-		if(emailAssignedTo!=null && emailAssignedTo.length()>0)
-			this.emailAssignedTo = emailAssignedTo;
+		this.emailAssignedTo = emailAssignedTo;
 	}
 
 	public boolean getDone() {
@@ -127,8 +95,7 @@ public class TaskInfo extends Model
 	}
 
 	public void setStartDate(String startDate) {
-		if(startDate.length()>0)
-			this.startDate = startDate;
+		this.startDate = startDate;
 	}
 
 	public String getEndDate() {
@@ -136,11 +103,16 @@ public class TaskInfo extends Model
 	}
 
 	public void setEndDate(String endDate) {
-		if(endDate.length()>0)
-			this.endDate = endDate;
+		this.endDate = endDate;
 	}
 
+	public int getPoints() {
+		return points;
+	}
 
+	public void setPoints(int points) {
+		this.points = points;
+	}
 
 	public String getCreatedBy() {
 		return createdBy;
@@ -150,6 +122,14 @@ public class TaskInfo extends Model
 		this.createdBy = createdBy;
 	}
 
-
-
+	@Required(message = "validation.required.emphasis")
+	//@DateTime(pattern = "mm/dd/yyyy hh:mm")
+	private String startDate;
+	
+	@Required(message = "validation.required.emphasis")
+	//@DateTime(pattern = "mm/dd/yyyy hh:mm")
+	private String endDate;
+	
+	@Required
+	private int points;
 }
